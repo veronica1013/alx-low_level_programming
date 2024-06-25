b6680a1583edba11ba44a1dde3fd83d0369741e9
veronica1013 <veronica.isiaho@gmail.com>
2024-06-25 14:23:43 -0700

    A program that prints _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
 /*
  * A program that prints _putchar
+ *#include <unistd.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
-int main(void)
+int _putchar(char c)
 {
-    putchar('_');
-    putchar('p');
-    putchar('u');
-    putchar('t');
-    putchar('c');
-    putchar('h');
-    putchar('a');
-    putchar('r');
-    putchar('\n');
+	return (write(1, &c, 1));
+}
 
