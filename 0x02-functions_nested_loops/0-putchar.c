32b5c327fca42f5677096cbdb8ebdcaa036b609e
veronica1013 <veronica.isiaho@gmail.com>
2024-06-25 14:18:53 -0700

    A program that prints _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/*
+ * A program that prints _putchar
+ */
+int main(void)
+{
+    putchar('_');
+    putchar('p');
+    putchar('u');
+    putchar('t');
+    putchar('c');
+    putchar('h');
+    putchar('a');
+    putchar('r');
+    putchar('\n');
+
