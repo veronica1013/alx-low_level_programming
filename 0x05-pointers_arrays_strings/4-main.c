193b3b70fb43dcc386922636734f9b3e57a58646
veronica1013 <veronica.isiaho@gmail.com>
2024-07-01 12:50:40 -0700

    A function that prints string in reverse

---
+++ 0x05-pointers_arrays_strings/4-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    char *str;
+
+    str = "This is s function that prints string in reverse";
+    print_rev(str);
+    return (0);
+}
